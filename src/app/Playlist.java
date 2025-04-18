da8b70524bfca04c3a9367f78bae1461919308b7
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-04-18 12:13:16 +0330

    complete methods in playlist class

--- src/app/Playlist.java
+++ src/app/Playlist.java
@@ -1,20 +1,106 @@
 package app;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Playlist {
     private User owner;
     private String title;
     private ArrayList<Music> playlist = new ArrayList<>();
 
-    void editTitle(String password, String title) {}
+    public Playlist(String title, User owner) {
+        setTitle(title);
+        this.owner = owner;
+    }
 
-    void addMusic(String password, Music music) {}
+    public void editTitle(String password, String title) {
+        if (!password.equals(owner.getPassword())) {
+            throw new InvalidOperationException("Error: \nPassword does not match");
+        }
 
-    void removeMusic(String password, Music music) {}
+        setTitle(title);
+    }
 
-    void searchInPlaylist () {}
+    public void addMusic(String password, Music music) {
+        if (!password.equals(owner.getPassword())) {
+           throw new InvalidOperationException("Error: \nPassword does not match");
+        }
 
-    void playPlaylist () {}
+        if (playlist.contains(music)) {
+            throw new InvalidOperationException("Error: \nMusic already exists in playlist");
+        }
+
+        playlist.add(music);
+    }
+
+    public void removeMusic(String password, Music music) {
+        if (!password.equals(owner.getPassword())) {
+            throw new InvalidOperationException("Error: \nPassword does not match");
+        }
+
+        if (playlist.contains(music)) {
+            playlist.remove(music);
+        }
+
+        throw new InvalidOperationException("Error: \nMusic does not exist in playlist");
+    }
+
+    public Music searchInPlaylist (String title, String singer) {
+        if (playlist.isEmpty()) {
+            throw new InvalidOperationException("Error: \nPlaylist is empty");
+        }
+
+        for (Music music : playlist) {
+            if (title.equals(music.getTitle()) && singer.equals(music.getSinger().getUsername())) {
+                return music;
+            }
+        }
+
+        return null;
+    }
+
+    public ArrayList<Music> searchInPlaylist (String title) {
+        ArrayList<Music> musics = new ArrayList<>();
+
+        if (playlist.isEmpty()) {
+            throw new InvalidOperationException("Error: \nPlaylist is empty");
+        }
+
+        for (Music music : playlist) {
+            if (title.equals(music.getTitle())) {
+                musics.add(music);
+            }
+        }
+
+        return musics;
+    }
+
+    public void playPlaylist () {
+        if (playlist.isEmpty()) {
+            throw new InvalidOperationException("Error: \nPlaylist is empty");
+        }
+
+        for (Music music : playlist) {
+            Music.play(music);
+        }
+
+    }
+
+    public void shuffle() {
+        if (playlist.isEmpty()) {
+            throw new InvalidOperationException("Error: \nPlaylist is empty");
+        }
+
+        ArrayList<Music> musics = new ArrayList<>(playlist);
+
+        Random random = new Random();
+
+        while (!musics.isEmpty()) {
+            int index = random.nextInt(musics.size());
+            Music music = musics.remove(index);
+            Music.play(music);
+        }
+
+    }
 
 }
