49d103aec2466c63e3927ef87b0abf78b1ed37e9
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-04-18 09:13:35 +0330

    add premium behavior class

---
+++ src/app/PremiumBehavior.java
@@ -0,0 +1,21 @@
+package app;
+
+public class PremiumBehavior implements UserBehavior {
+    private int month;
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+
+    }
+
+    @Override
+    public void playMusic(Music music) {
+
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+
+    }
+
+}
